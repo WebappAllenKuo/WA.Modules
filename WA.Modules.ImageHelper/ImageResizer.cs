34976a61eb84fa02f7e1f3781d76977d4db7986d
LillianLin <a888gogogo@yahoo.com.tw>
2021-04-21 23:16:50 +0800

    Initial commit

---
+++ WA.Modules.ImageHelper/ImageResizer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WA.Modules.ImageHelper
+{
+    public class ImageResizer
+    {
+    }
+
+    public class ImageResizeSettings
+    {
+        private Dictionary<string, ImageResizeSetting> Settings = new Dictionary<string, ImageResizeSetting>();
+
+        public ImageResizeSetting GetSetting(string key)
+        {
+            return this.Settings.ContainsKey(key)
+                ? Settings[key]
+                : new ImageResizeSetting();
+        }
+    }
+
+    public class ImageResizeSetting
+    {
+        public int NewWidth { get; set; }
+        public int newHeight { get; set; }
+        public string NewLocation { get; set; }
+    }
+}
